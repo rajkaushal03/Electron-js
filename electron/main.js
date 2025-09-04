fd02793437177dd9ba46dd1ca9079a74001e6c77
Swayam Kaushal <101446225+rajkaushal03@users.noreply.github.com>
2025-09-04 11:48:31 +0530

    two electron window ui and response

--- electron/main.js
+++ electron/main.js
@@ -28,28 +28,29 @@ function createWindows() {
     },
   });
   win.setContentProtection(true);
-  win.loadFile("index.html");
+  win.loadFile(path.join(__dirname, "../index.html"));
+
 
   // 🔹 Second window (for responses, full screen as an example)
-  responseWin = new BrowserWindow({
-    title: "kluely-responses",
-    width:500, // full width
-    height:300, // full height
-    x: Math.round((width - 500) / 2), // start from left edge
-    y: 60, // start from top edge
-    transparent: false, //this will make the application transparent 
-    frame: false, //this will remove the file,edit fframe from the application
-    alwaysOnTop: true, //this will always put the application on top
-    skipTaskbar: true, //this will remove appicon from taskbar
-    resizable: false, //this will not allow user to resize the applicationWorld!
-    fullscreen: false, //this will restrict user to go fullscreen
-    backgroundColor: "rgba(0, 0, 0, 0.3)",  
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
-  responseWin.setContentProtection(true);
-  responseWin.loadFile("responses.html");
+  // responseWin = new BrowserWindow({
+  //   title: "kluely-responses",
+  //   width:500, // full width
+  //   height:300, // full height
+  //   x: Math.round((width - 500) / 2), // start from left edge
+  //   y: 60, // start from top edge
+  //   transparent: false, //this will make the application transparent 
+  //   frame: false, //this will remove the file,edit fframe from the application
+  //   alwaysOnTop: true, //this will always put the application on top
+  //   skipTaskbar: true, //this will remove appicon from taskbar
+  //   resizable: false, //this will not allow user to resize the applicationWorld!
+  //   fullscreen: false, //this will restrict user to go fullscreen
+  //   backgroundColor: "rgba(0, 0, 0, 0.3)",  
+  //   webPreferences: {
+  //     preload: path.join(__dirname, "preload.js"),
+  //   },
+  // });
+  // responseWin.setContentProtection(true);
+  // responseWin.loadFile("responses.html");
 }
 
 app.whenReady().then(() => {
@@ -62,7 +63,7 @@ app.whenReady().then(() => {
   });
 });
 
-app.on("window-all-closed", () => {
+app.on("window-all-closed", () => { 
   if (process.platform !== "darwin") {
     app.quit();
   }
