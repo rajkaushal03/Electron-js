a9af66636357c9f21ddf17ec6866b9d38cca72b6
Swayam Kaushal <101446225+rajkaushal03@users.noreply.github.com>
2025-09-04 11:20:15 +0530

    two electron window ui and response

--- main.js
+++ main.js
@@ -1,30 +1,63 @@
 const { app, BrowserWindow } = require("electron/main");
+const { screen } = require("electron");
 const path = require("node:path");
 
-function createWindow() {
-  const win = new BrowserWindow({
+let win;
+let responseWin;
+
+function createWindows() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  // 🔹 First window (your original one)
+  win = new BrowserWindow({
+    title: "kluely",
     width: 300,
-    height: 300, 
-    transparent:false, //this will make the application transparent 
-    frame:false, //this will remove the file,edit fframe from the application
-    alwaysOnTop:false,//this will always put the application on top
-    skipTaskbar:true,//this will remove appicon from taskbar
-    resizable:false, //this will not allow user to resize the application
-    fullscreen:false, //this will restrict user to go fullscreen
+    height: 20,
+    x: Math.round((width - 300) / 2), // center horizontally
+    y: 20, // stick to the top
+    transparent: false, //this will make the application transparent 
+    frame: false, //this will remove the file,edit fframe from the application
+    alwaysOnTop: true, //this will always put the application on top
+    skipTaskbar: true, //this will remove appicon from taskbar
+    resizable: false, //this will not allow user to resize the applicationWorld!
+    fullscreen: false, //this will restrict user to go fullscreen
+    backgroundColor: "rgba(0, 0, 0, 0.3)",  
+    // opacity: 0.5,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
-
+  win.setContentProtection(true);
   win.loadFile("index.html");
+
+  // 🔹 Second window (for responses, full screen as an example)
+  responseWin = new BrowserWindow({
+    title: "kluely-responses",
+    width:500, // full width
+    height:300, // full height
+    x: Math.round((width - 500) / 2), // start from left edge
+    y: 60, // start from top edge
+    transparent: false, //this will make the application transparent 
+    frame: false, //this will remove the file,edit fframe from the application
+    alwaysOnTop: true, //this will always put the application on top
+    skipTaskbar: true, //this will remove appicon from taskbar
+    resizable: false, //this will not allow user to resize the applicationWorld!
+    fullscreen: false, //this will restrict user to go fullscreen
+    backgroundColor: "rgba(0, 0, 0, 0.3)",  
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  responseWin.setContentProtection(true);
+  responseWin.loadFile("responses.html");
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  createWindows();
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+      createWindows();
     }
   });
 });
